3c8630c8673ac8b6b3e9869c1dd47e1c252d4df6
Oleg <oleg@MacBook-Air-Oleg.local>
2025-03-05 13:55:09 +0900

    Пробую подключить MySQL

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'subway-lab/sto-site',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2a4af9c63086dc2adfcf72cb98f1158e403a9cfd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'subway-lab/sto-site' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2a4af9c63086dc2adfcf72cb98f1158e403a9cfd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
